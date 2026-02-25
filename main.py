675af470cfa99e890397aba77cf4619d32515f63
aditya61006 <adityachoudhary61006@gmail.com>
2026-02-26 00:23:15 +0530

    jh

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import speech_recognition as sr
 import webbrowser
-import pyttsx3 
-
+import pyttsx3
+import songlibrary
+import  requests
  #speech recognition functionality deti h
 
 def speak(text):
@@ -9,15 +10,41 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+newsapi = "78fb0a269bd7408ab69221679ef307d5"
+
 def processCommand(command):
-    if( command.lower()== "open youtube"):
+    if "open youtube" in command.lower():
         print("Opening Youtube")
         speak("Opening Youtube")
         webbrowser.open("www.youtube.com")
-    elif( command.lower()== "instagram"):
+    elif"open instagram" in command.lower():
         print("Opening Instagram")
         speak("Opening Instagram")
         webbrowser.open("www.instagram.com")
+    elif "open google " in command.lower():
+        print("Opening google")
+        speak("Opening google")
+        webbrowser.open("www.google.com")
+    elif "open my github" in command.lower():
+        print("Opening github")
+        speak("Opening github")
+        webbrowser.open("https://github.com/")
+    elif  command.lower().startswith("play"):
+        print(command.lower())
+        song = command.lower().split(" ")[1]
+        link = songlibrary.music[song]
+        webbrowser.open(link)
+    elif  "news" in command.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")
+        if r.status_code == 200:
+            data = r.json()
+            articles = data.get('articles',[])
+            for article in articles:
+                print(article['title'])
+                speak(article['title'])
+
+
+
 
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
@@ -45,11 +72,14 @@ while True:
                 audio = r.listen(source)
                 command = r.recognize_google(audio)
                 print("you :",command)
+
+                if "terminate" in command.lower():
+                    print("Thankyou sir , program terminated")
+                    speak("Thankyou sir .Program Terminated")
+                    break
+
                 processCommand(command)
-        elif(word.lower()== "jarvis terminate"):
-            print("Thankyou sir , program terminated")
-            speak("Thankyou sir .Program Terminated")
-            break
+              
         
     except Exception as e:
         print(e)
---
+++ songlibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "jogi" : "https://youtu.be/AsEd5OAys9E?si=DzC9ADelmCllGt5Z",
+    "song" : "https://youtu.be/G8nlhcmDXNE?si=aBq4iJkfV3o3WMZb",
+     "sharmeeli" : "https://youtu.be/AXeB1vrz7II?si=KhvVqZTyZzmNPo0l",
+        "thinking": "https://youtu.be/CbvjhmFVMvA?si=0hOguCObKP_Vumza",
+        "dooron dooron": "https://youtu.be/LV_wiOhO40Q?si=4t2IttLLjYpzL0Mx"
+}
